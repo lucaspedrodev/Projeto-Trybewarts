3c72a45f85c37b3ddf9d9d28cf70a5434f714083
José Filipe Vieira Furieri <jf.furieri@ymail.com>
2022-07-14 12:17:48 -0300

    requisito 17,18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.querySelector('.email');
 const senha = document.querySelector('.senha');
 const botao = document.querySelector('.botao');
+const check = document.querySelector('#agreement');
+document.querySelector('#submit-btn').disabled = true;
 
 function validacao() {
   const valor1 = email.value;
@@ -13,3 +15,14 @@ function validacao() {
   }
 }
 botao.addEventListener('click', validacao);
+
+function ativarBotao() {
+  const conteudo = check.value;
+  if (conteudo !== null && conteudo !== '') {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
+}
+
+document.getElementById('agreement').addEventListener('input', ativarBotao);
